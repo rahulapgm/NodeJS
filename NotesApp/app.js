16506630b0f57b946a8679548f06c9ebaf204a04
devpgm <devpgm@hotmail.com>
2018-08-19 06:41:51 -0400

    add coded changes

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -1,12 +1,16 @@
 console.log("App Started");
 
-const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
 
 var userInfo = os.userInfo();
-//console.log(userInfo)
+var fileName ="greetings.txt";
 
-fs.appendFileSync('greetings.txt',`Hello ${userInfo.username}! You're ${notes.age}.`);
+const res = notes.addNote(fileName, `Hello ${userInfo.username}! You're ${notes.age}.`);
+
+// var userInfo = os.userInfo();
+// //console.log(userInfo)
+
+// fs.appendFileSync('greetings.txt',`Hello ${userInfo.username}! You're ${notes.age}.`);
 
 
--- NotesApp/notes.js
+++ NotesApp/notes.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+
 console.log("started notes module");
 // console.log(module);
 module.exports.age = 25;
+module.exports.addNote = (fileName, text) => {
+  fs.appendFileSync(fileName, text);
+}
